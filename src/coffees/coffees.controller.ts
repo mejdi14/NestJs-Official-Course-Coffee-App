6562fc726a2883f9b1744d08c871fb5b0a5b118a
unknown <mejdiclubiste@gmail.com>
2020-09-13 09:42:50 +0100

    creating get and post reaquests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get()
-  findAll() {
-    return 'this is my first controller'
+  @Get(':id')
+  findAll(@Param() params) {
+    return `showing the number ${params.id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
