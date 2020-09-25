fc78634b7beecfac29ab63fd9e4a1284959b01da
unknown <mejdiclubiste@gmail.com>
2020-09-25 14:41:32 +0100

    orginaze code in module

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from '../entities/coffee.entitie';
 
 @Injectable()
@@ -19,7 +19,7 @@ export class CoffeesService {
   findOne(id: string) {
     const coffee = this.coffees.find(item => item.id === +id);
     if (!coffee) {
-      throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND)
+      throw new NotFoundException(`Coffee #${id} not found`)
     }
     return coffee
   }
