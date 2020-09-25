09bbb4d5f8623f72ba4975b6080ead40ed23485d
unknown <mejdiclubiste@gmail.com>
2020-09-25 17:19:31 +0100

    adding docker compose yml file

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
-    forbidNonWhitelisted: true
+    forbidNonWhitelisted: true,
+    transform: true
   }));
   await app.listen(3000);
 }
