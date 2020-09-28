0bd910506da2a7b442ee88123b10789f5001aec9
unknown <mejdiclubiste@gmail.com>
2020-09-28 23:28:27 +0100

    working with pagination and it's awesome

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Flavor } from './entities/flavor.entitie';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 
 @Injectable()
 export class CoffeesService {
@@ -17,9 +18,12 @@ export class CoffeesService {
 
   }
 
-  findAll() {
+  findAll(paginationQuery: PaginationQueryDto) {
+    const { limit, offset }= paginationQuery;
     return this.coffeeRepository.find({
-      relations: ['flavors']
+      relations: ['flavors'],
+      take: limit,
+      skip: offset
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
-    transform: true
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true
+    }
   }));
   await app.listen(3000);
 }
