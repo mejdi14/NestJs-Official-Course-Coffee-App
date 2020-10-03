c2b018c894557e35f2e701b4f7d001f15aa8d669
unknown <mejdiclubiste@gmail.com>
2020-10-03 10:27:02 +0100

    installing swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,12 +14,11 @@ import { ConfigModule } from '@nestjs/config';
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-
-      password: 'pass123',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }), CoffeeRatingModule],
